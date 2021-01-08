328c12dada89880a2530d75688d2ef136f3bcc2c
Mildophin <viricel.milan@gmail.com>
2021-01-08 12:47:53 +0100

    version de base

---
+++ main.py
@@ -0,0 +1,26 @@
+from requests import Request, Session
+from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
+import json
+
+url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+parameters = {
+  'start':'1',
+  'limit':'1',
+  'convert':'USD'
+}
+headers = {
+  'Accepts': 'application/json',
+  'X-CMC_PRO_API_KEY': '257c3523-f7a8-4866-8fcf-cee9da4bfe8d',
+}
+
+session = Session()
+session.headers.update(headers)
+
+try:
+  response = session.get(url, params=parameters)
+  data = json.loads(response.text)
+  price = data['data'][0]['quote']['USD']['price']
+  price = int(price)
+  print("Prix du bitcoin: " + str(price) + " $")
+except (ConnectionError, Timeout, TooManyRedirects) as e:
+  print(e)
